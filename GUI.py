8504f81c7cff38d16b50f748b1c1949c68aabd87
Pratyush <87978502+Pratyush-Remon@users.noreply.github.com>
2022-01-03 16:28:33 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,16 +1,17 @@
 from tkinter import *
-from tkinter import ttk                        #i guess i didnt used it but nevermind
+from tkinter import ttk                                #not using it, i guess but let it be
 from difflib import get_close_matches
 import json
 
 root=Tk()
 
 root.title("Dictionary")
-root.iconbitmap("Martz90-Circle-Books.ico")          #path for the icon file
+root.iconbitmap("path\\Martz90-Circle-Books.ico")
 root.config(background='#1D1C1A')
 root.geometry('600x400+50+50')
 
-data=json.load(open("dic.json"))                     #path for the dic.json
+data=json.load(open("path\\dic.json"))
+
 
 
 
@@ -20,25 +21,30 @@ def translate(w):
     if w in data:
         lable4=Label(root,text=data[w])
         lable4.grid(column=0,row=2)
-    elif len(get_close_matches(w,data.keys()))>0:
-        root2=Toplevel(root)
-        labl1=Label(root2,text=("Do you mean %s instead?[Y/N]"%get_close_matches(w,data.keys())[0]))
-        labl1.grid(column=0,row=0)
-        res=Entry(root2,width=15,borderwidth=3)
-        res.grid(column=3,row=1)
+    elif len(get_close_matches(w,data.keys()))>0:                                 #condition if the word is misspelt 
+        root2=Toplevel(root)                                                      #second window
+        root2.config(background='#404040')
+        root2.iconbitmap("path\\Martz90-Circle-Books.ico")
+        root2.geometry('600x400+50+50')
         
-        if res.get()=='Y':
-            lable2=Label(root2,text=(get_close_matches(w,data.keys())[0]))
-            lable2.grid(column=0,row=2)
-        elif res.get()=='N':
-            lable2=Label(root2,text="Word don't exists in our dictionary")
-            lable2.grid(column=0,row=2)
-        else:
-            lable2=Label(root2,text="We can't understand your response")
-            lable2.grid(column=0,row=2)
-    else:
-       lable3=Label(root,text="Word doesn't exist in our dictionary")
-       lable3.grid(column=0,row=2)
+        def true():
+            x=Label(root2,text=data[get_close_matches(w,data.keys())[0]])
+            x.grid(column=0, row=3)
+        
+        def no():
+            v= Label(root2,text="We don't have your word in our dictionary.")
+            v.grid(column=0,row=3)
+            
+        labl1=Label(root2,text=("Do you mean %s instead?[Yes/No]"%get_close_matches(w,data.keys())[0]))
+        labl1.grid(column=0,row=0)
+        
+        yes=Button(root2,text='Yes', command= true)
+        yes.grid(column=0,row=1)
+        
+        nope=Button(root2,text='No',command= no)
+        nope.grid(column=1,row=1)
+        
+
 
 e=Entry(root,width=15,borderwidth=3)             #to get input
 e.grid(column=3,row=0,padx=6,pady=2)
